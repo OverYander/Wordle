3de74e30afbb67d5fc1997fa3dd9aa33538945ad
OverYander <gavinchen8706@gmail.com>
2026-02-17 01:05:24 -0500

    finished input body

--- script.mjs
+++ script.mjs
@@ -1,24 +1,27 @@
-// import data from "./words.json" with { type: "json" };
 
-// const {words} = data;
-// console.log(words[0]);
-// console.log(words[1]);
+// Auto generate grid for input cells
+const rows = 6;
+const cols = 5;
 
-import fs from 'fs'
+const inputRows = document.querySelector('.inputRows');
+for(let i = 0; i < rows * cols; i++) {
+    const inputCell = document.createElement('li');
+    inputCell.classList.add('inputCell');
+    inputRows.appendChild(inputCell);
+}
 
-fs.readFile('words.json', 'utf8', (err, data) => {
-    if(err) {
-        console.log('read file error')
-        return;
-    }
+const keyboardStr = 'QWERTYUIOPASDFGHJKLZXCVBNM'; 
+// row 1 -10, row 2 - 9, row 3 - 7
 
-    const allWords = JSON.parse(data).words; 
-    // console.log(typeof allWords)
-    // console.log(allWords.length)
+const kbRow1 = document.querySelector('.kbRow1');
+const kbRow2 = document.querySelector('.kbRow2');
+const kbRow3 = document.querySelector('.kbRow3');
 
-    
-})
-
-/*
-node script.mjs
-*/
+for(let i = 0; i < keyboardStr.length; i++) {
+    const keyboardCell = document.createElement('li');
+    keyboardCell.classList.add('keyboardCell');
+    keyboardCell.textContent = keyboardStr[i];
+    if(i < 10) kbRow1.appendChild(keyboardCell);
+    else if(i < 19) kbRow2.appendChild(keyboardCell);   
+    else kbRow3.appendChild(keyboardCell);
+}
