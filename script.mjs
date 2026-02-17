a8522b3abeb9c3b3aca7ee66eeae0a7d12bfdb92
OverYander <gavinchen8706@gmail.com>
2026-02-17 00:25:11 -0500

    reformatted navbar and added filereader

--- script.mjs
+++ script.mjs
@@ -1,5 +1,24 @@
-import data from "./words.json" with { type: "json" };
+// import data from "./words.json" with { type: "json" };
 
-const {words} = data;
-console.log(words[0]);
-console.log(words[1]);
+// const {words} = data;
+// console.log(words[0]);
+// console.log(words[1]);
+
+import fs from 'fs'
+
+fs.readFile('words.json', 'utf8', (err, data) => {
+    if(err) {
+        console.log('read file error')
+        return;
+    }
+
+    const allWords = JSON.parse(data).words; 
+    // console.log(typeof allWords)
+    // console.log(allWords.length)
+
+    
+})
+
+/*
+node script.mjs
+*/
